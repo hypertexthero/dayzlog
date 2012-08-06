e33e568ca72d79a19d67c22e650bcc4ded4143fe
Simon Griffee <simon@hypertexthero.com>
2012-08-06 09:03:52 +0200

    deleted files

--- apps/nomadblog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from nomadblog.models import Post, Blog, Category, BlogUser
-
-admin.site.register(Blog)
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(BlogUser)
