f95d7a4d0a89e3bc5389bba7f95755195ac9d663
Simon Griffee <simon@hypertexthero.com>
2012-09-06 18:49:17 +0200

    debug username in django-voting view

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from dayzlog.apps.blog.models import Post, IS_PUBLIC
 from dayzlog.apps.profiles.models import Profile
 
+from django.contrib.auth.models import User
+
 # =voting
 # http://code.google.com/p/django-voting/wiki/RedditStyleVoting
 from django.views.generic.list_detail import object_list
@@ -13,6 +15,7 @@ post_dict_vote = {
     # 'queryset': Post.objects.filter(status=IS_PUBLIC),
     'template_object_name': 'post',
     'slug_field': 'slug',
+    # 'extra_context': {"username": User},
     'template_name': 'blog/post_confirm_vote.html',
     'allow_xmlhttprequest': 'true',
 }
@@ -42,7 +45,11 @@ urlpatterns = patterns('',
     url(r'^(?P<action>draft|public)/(?P<id>\d+)/$', 'dayzlog.apps.blog.views.change_status', name='blog_change_status'),
     url(r'^delete/(?P<id>\d+)/$', 'dayzlog.apps.blog.views.delete', name='blog_delete'), 
     # =voting
+    # =todo: figure out why vote doesn't work for logged out users after logging in and accessing post_voting view
+    # http://code.google.com/p/django-voting/source/browse/trunk/voting/views.py
+    # url(r'^(?P<username>[\w\._\-]+)/(?P<slug>[-\w]+)/(?P<direction>up|down|clear)vote/?$', vote_on_object, post_dict_vote, name='post_voting'),
     url(r'^(?P<slug>[-\w]+)/(?P<direction>up|down|clear)vote/?$', vote_on_object, post_dict_vote, name='post_voting'),
+
     # needs to come after the above otherwise post detail url doesn't work
     
     url(r'^(?P<username>[\w\._\-]+)/(?P<slug>[-\w]+)/$', 'dayzlog.apps.blog.views.blog_user_post_detail', post_dict, name='blog_user_post_detail'),
