e905dc413e51501859faaee369c3de501699bafa
Simon Griffee <simon@hypertexthero.com>
2012-09-16 15:53:20 +0200

    jquery pagedown markdown preview in post add/edit forms

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -5,12 +5,14 @@ from django.utils.translation import ugettext_lazy as _
 from dayzlog.apps.blog.models import Post, IS_DRAFT, IS_PUBLIC
 
 class PostForm(forms.ModelForm):
-    
+
     class Meta:
         model = Post
         exclude = ('author', 'slug', 'created_at', 'updated_at', 'publish', 
                     'content_html')
-    
+
     def __init__(self, request, *args, **kwargs):
         self.user = request.user
         super(PostForm, self).__init__(request.POST or None, *args, **kwargs)
+        self.fields['content_markdown'].widget.attrs['class'] = 'wmd-input'
+        self.fields['content_markdown'].widget.attrs['id'] = 'wmd-input'
