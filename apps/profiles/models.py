3751d1e04ccf2ec23b2dfd4049c53d44fc011c72
Simon Griffee <simon@hypertexthero.com>
2012-09-16 12:09:32 +0200

    profile form text update

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -11,10 +11,10 @@ from datetime import datetime
 from easy_thumbnails.fields import ThumbnailerImageField
 
 class Profile(ProfileBase):
-    name = models.CharField(_("Day Z In-Game Profile Extended Name"), max_length=50, null=True, blank=True, help_text="The extended name you want to display in-game as well as at dayzlog.com. If you want this to match the in-game player name that appears in the roster when pressing the P key, write that same name here.")
-    player_id = models.CharField(_("Player ID"), max_length=20, null=True, blank=True, help_text="<b>Must match your Day Z in-game player id exactly <a href='/faq/#playerid'>(what is my player id?)</a>. Required to make your profile information appear within Day Z.</b>")
-    email = models.EmailField(_("Email"), blank=True, help_text="An email address you want others to see, if any.")
-    im = models.CharField(_("im"), max_length=50, null=True, blank=True, help_text="An instant messaging address you want others to see, if any.")
+    name = models.CharField(_("Day Z In-Game Profile Extended Name"), max_length=50, null=True, blank=True, help_text="The extended name you want to display in-game as well as at dayzlog.com. If you want this to match the player name that appears in the in-game roster, write that same name here.")
+    player_id = models.CharField(_("Player ID"), max_length=20, null=True, blank=True, help_text="<b>Must match your Day Z in-game Player ID exactly</b> <a href='/faq/#playerid'>(what is my Player ID?)</a>. Required to make your profile information appear within Day Z.")
+    email = models.EmailField(_("Email"), blank=True, help_text="An email address you want to display.")
+    im = models.CharField(_("im"), max_length=50, null=True, blank=True, help_text="An instant messaging handle you want to display.")
     remark = models.TextField(_("remark"), null=True, blank=True, help_text="Any remarks you would like to share?")
     player_img = models.ImageField(_("Image"), upload_to="player_img/%Y/%m/%d/", blank=True, help_text="Will be resized to 260x180 pixels, if larger.") # no player images yet
     # player_img = ProcessedImageField(_("Image"), upload_to="player_img/%Y/%m/%d/", blank=True) # no player images yet
